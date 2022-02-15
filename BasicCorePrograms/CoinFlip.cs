16b6cbcab6fbb3425d1a7395bd7fe44696f1d34c
Devendra533 <97461548+Devendra533@users.noreply.github.com>
2022-02-15 15:53:40 +0530

    Added Flip Coin Logic

---
+++ BasicCorePrograms/CoinFlip.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class CoinFlip
+    {
+        public void PercentageOfCoinFlip()
+        {
+            int HEADS = 0;
+            int TAILS = 0;
+            int Count = 0;
+            int PercentageOfHeads = 0;
+            int PercentageOfTails = 0;
+            Random flip = new Random();
+            while (Count < 100)
+            {
+
+
+                int flipCheck = flip.Next(0, 2);
+                if (flipCheck < 0.5)
+                {
+                    Console.WriteLine("HEADS");
+                    HEADS++;
+                    Count++;
+                }
+                else
+                    Console.WriteLine("TAILS");
+                TAILS++;
+                Count++;
+            }
+            PercentageOfHeads = HEADS *( Count / 100);
+            PercentageOfTails = TAILS * (Count / 100);
+            Console.WriteLine("PERCENTAGE OF HEADS " + PercentageOfHeads);
+            Console.WriteLine("PERCENTAGE OF TAILS " + PercentageOfTails);
+
+        }
+    }
+}
