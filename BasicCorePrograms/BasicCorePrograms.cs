6711739968a50cff1b86630262dd607aa0750f49
Devendra533 <97461548+Devendra533@users.noreply.github.com>
2022-02-26 17:49:01 +0530

    Added Alphabet Is Vowel Or Consonant Logic

--- BasicCorePrograms/BasicCorePrograms.cs
+++ BasicCorePrograms/BasicCorePrograms.cs
@@ -165,5 +165,23 @@ namespace BasicCorePrograms
                 Console.WriteLine("Number is a Odd Number ");
 
         }
+
+        public void Alphabet()
+        {
+            char ch;
+
+            Console.WriteLine("Enter Any Character: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+
+                Console.WriteLine(" It is a Vowel ");
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant.");
+
+            }
+            Console.ReadLine();
+        }
     }
 }    
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,6 +8,6 @@ BasicCorePrograms .BasicCorePrograms flip = new BasicCorePrograms.BasicCoreProgr
 //int n = 9;
 //flip.PowerOf2(n);
 //flip.HarmonicNumber();
-flip.EvenOdd(); 
-
+//flip.EvenOdd(); 
+flip.Alphabet();
 Console.ReadKey();
